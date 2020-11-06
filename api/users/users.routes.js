a68eedf6cf12d17e652bb31bd8b51b3c480c3e11
poskachei <vposkachey@gmail.com>
2020-11-06 11:20:27 +0200

    fixed1

--- api/users/users.routes.js
+++ api/users/users.routes.js
@@ -7,7 +7,7 @@ UsersRouter.post('/users/register', UsersController.validateCreatedUser, upload.
 UsersRouter.post('/users/login', UsersController.validateCreatedUser, UsersController.signIn);
 UsersRouter.post('/users/logout', UsersController.authorize, UsersController.logOut);
 UsersRouter.get('/users/current', UsersController.authorize, UsersController.getUserСurrent);
-UsersRouter.patch('/user/avatar', UsersController.authorize, upload.single('avatar'), UsersController.minifyImage, UsersController.updateUser)
+UsersRouter.patch('/users/avatar', UsersController.authorize, upload.single('avatar'), UsersController.minifyImage, UsersController.updateUser)
 
 
 module.exports = UsersRouter;
